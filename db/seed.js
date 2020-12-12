904aa3a606aa8cc43dce56619b11a4a5fed6320b
Andrew <oleandy1221@gmail.com>
2020-12-12 12:29:57 -0600

    heroku

--- db/seed.js
+++ db/seed.js
@@ -175,20 +175,21 @@ const {
       console.log("Calling getUserById with 1");
       const albert = await getUserById(1);
       console.log("Result:", albert);
+      console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[0].id, {
+        tags: ["#youcandoanything", "#redfish", "#bluefish"]
+      });
+    console.log("Result:", updatePostTagsResult);
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
   
       console.log("Finished database tests!");
     } catch (error) {
       console.log("Error during testDB");
       throw error;
     }
-    console.log("Calling updatePost on posts[1], only updating tags");
-    const updatePostTagsResult = await updatePost(posts[0].id, {
-      tags: ["#youcandoanything", "#redfish", "#bluefish"]
-    });
-    console.log("Result:", updatePostTagsResult);
-    console.log("Calling getPostsByTagName with #happy");
-    const postsWithHappy = await getPostsByTagName("#happy");
-    console.log("Result:", postsWithHappy);
+    
   }
   
   
